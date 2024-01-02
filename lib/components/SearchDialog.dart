e29e64f2267fd8af6d7c830378c207447afa95bd
Elisei.Nicolae <nicolae.elisei1999@gmail.com>
2024-01-02 19:59:18 +0200

    fix: fix the Search Dialog on the top

--- lib/components/SearchDialog.dart
+++ lib/components/SearchDialog.dart
@@ -20,7 +20,6 @@ class _SearchDialogState extends State<SearchDialog> {
   @override
   void initState() {
     super.initState();
-    // Set focus on the text form field when the dialog is opened
     Future.delayed(const Duration(milliseconds: 100), () {
       FocusScope.of(context).requestFocus(_focusNode);
     });
@@ -30,7 +29,6 @@ class _SearchDialogState extends State<SearchDialog> {
   void _filterSearchResults(String query) {
     setState(() {
       searchResults = widget.stationsMediaItems.where((item) {
-        // Use a case-insensitive comparison for search
         return item.title.toLowerCase().contains(query.toLowerCase());
       }).toList();
     });
@@ -40,63 +38,55 @@ class _SearchDialogState extends State<SearchDialog> {
   Widget build(BuildContext context) {
     return Dialog(
       surfaceTintColor: Colors.transparent,
-      backgroundColor: Colors.white, // Set the background color to white
+      backgroundColor: Colors.white,
       shape: RoundedRectangleBorder(
         borderRadius: BorderRadius.circular(8.0),
       ),
-      child: SingleChildScrollView(
-        child: Container(
-          padding: const EdgeInsets.only(top: 6.0, left: 8.0, right: 2.0, bottom: 8.0),
-          child: Column(
-            mainAxisSize: MainAxisSize.min,
-            children: [
-              Row(
-                children: [
-                  Expanded(
-                    child: TextFormField(
-                      focusNode: _focusNode,
-                      onChanged: _filterSearchResults,
-                      decoration: const InputDecoration(
-                        hintText: 'Tastează numele stației..',
-                        border: OutlineInputBorder(),
-                        contentPadding: EdgeInsets.symmetric(horizontal: 8.0),
-                      ),
+      child: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            Row(
+              children: [
+                Expanded(
+                  child: TextFormField(
+                    focusNode: _focusNode,
+                    onChanged: _filterSearchResults,
+                    decoration: const InputDecoration(
+                      hintText: 'Tastează numele stației..',
+                      border: OutlineInputBorder(),
+                      contentPadding: EdgeInsets.symmetric(horizontal: 8.0),
                     ),
                   ),
-                  Container(
-                    margin: const EdgeInsets.only(top: 8, right: 0),
-                    child: CloseButton(
-                      color: Colors.grey[800],
-                      onPressed: () {
-                        Navigator.of(context).pop();
+                ),
+                CloseButton(
+                  color: Colors.grey[800],
+                  onPressed: () => Navigator.of(context).pop(),
+                )
+              ],
+            ),
+            const SizedBox(height: 16.0),
+            Flexible(
+              child: searchResults.isEmpty
+                  ? Center(child: Text('Nu am găsit nicio stație cu acest nume.'))
+                  : ListView.builder(
+                      itemCount: searchResults.length,
+                      itemBuilder: (context, index) {
+                        final item = searchResults[index];
+                        return GestureDetector(
+                          onTap: () {
+                            widget.audioHandler.playMediaItem(item);
+                            Navigator.of(context).pop();
+                          },
+                          child: ListTile(
+                            title: Text(item.title),
+                          ),
+                        );
                       },
                     ),
-                  )
-                ],
-              ),
-              const SizedBox(height: 16.0), // Spacing
-              // List of results
-              ListView.builder(
-                physics: const NeverScrollableScrollPhysics(),
-                shrinkWrap: true,
-                itemCount: searchResults.length, // Replace with your actual data count
-                itemBuilder: (context, index) {
-                  final item = searchResults[index];
-
-                  return GestureDetector(
-                    onTap: () {
-                      widget.audioHandler.playMediaItem(item);
-                      Navigator.of(context).pop();
-                    },
-                    child: ListTile(
-                      title: Text(searchResults[index].title),
-                      // Add more content or customize as needed
-                    ),
-                  );
-                },
-              ),
-            ],
-          ),
+            ),
+          ],
         ),
       ),
     );
