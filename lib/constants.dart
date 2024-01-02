6743cd050a83fd90e5e5a320ed2d56f19d838fb2
Elisei.Nicolae <nicolae.elisei1999@gmail.com>
2024-01-02 23:14:03 +0200

    fix: bug start/stop using STATIC_MP3_URL

--- lib/constants.dart
+++ lib/constants.dart
@@ -3,4 +3,6 @@ class CONSTANTS {
   static String GRAPHQL_AUTH = "Token public";
   static String DEFAULT_STATION_THUMBNAIL_URL = "";
   static String IMAGE_PROXY_PREFIX = "";
+  static String STATIC_MP3_URL =
+      "https://radio-crestin.s3.eu-central-1.amazonaws.com/media/public/simple_mp3_audio.mp3";
 }
