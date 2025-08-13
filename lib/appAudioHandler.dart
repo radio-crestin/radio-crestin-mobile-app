a9a46c6901190bfab24bab3039094565d1747a75
Elisei_Nicolae <nicolae.elisei1999@gmail.com>
2025-08-13 10:57:42 +0300

    fix: add a small delay of 100ms to wait (there was a bug on android with HLS on auto-start).

--- lib/appAudioHandler.dart
+++ lib/appAudioHandler.dart
@@ -173,6 +173,7 @@ class AppAudioHandler extends BaseAudioHandler {
   Future<void> playStation(Station station) async {
     _log('playStation($station)');
     await selectStation(station);
+    await Future.delayed(const Duration(milliseconds: 100));
 
     return play();
   }
